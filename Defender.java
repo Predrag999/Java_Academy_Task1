9de902584316c03f37486f3a0efc84036524925c
Predrag999 <102669453+Predrag999@users.noreply.github.com>
2022-07-01 10:30:05 +0300

    Update Defender.java

--- Defender.java
+++ Defender.java
@@ -1,22 +1,22 @@
 package org.example;
 
 public class Defender extends Warrior {
-    private static final int ATTACK = 3;
+//    private static final int ATTACK = 3;
     private static final int DEFENSE = 2;
     public Defender(){
-        super(60);
+        super(60,3);
     }
-    @Override
-    public  int getAttack(){
-        return ATTACK;
-    }
+//    @Override
+//    public  int getAttack(){
+//        return ATTACK;
+//    }
 
     public  static int getDefense(){
         return DEFENSE;
     }
     @Override
-    protected void hittedBy(canAttack attacker){
-        super.hittedBy(() -> Math.max(0, attacker.getAttack() - getDefense()));
+    protected int hittedBy(canAttack attacker){
+         return super.hittedBy(() -> Math.max(0, attacker.getAttack() - getDefense()));
     }
 
 }
